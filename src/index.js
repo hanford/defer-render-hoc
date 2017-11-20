8e59b67cf80552c37a5b47efdd3fff8d670b4d5e
hanford <jackhanford@gmail.com>
2017-11-19 18:39:05 -0800

    add hoist-non-react-statics

--- src/index.js
+++ src/index.js
@@ -1,25 +1,23 @@
 import React, { PureComponent } from 'react'
 import raf from 'raf'
+import hoistNonReactStatic from 'hoist-non-react-statics'
 
 /**
  * Allows two animation frames to complete to allow other components to update
  * and re-render before mounting and rendering an expensive `WrappedComponent`.
  */
 export default function deferComponentRender (WrappedComponent) {
-  return class DeferredRenderWrapper extends PureComponent {
-    constructor(props, context) {
-      super(props, context)
-      this.state = { shouldRender: false }
+  class DeferredRenderWrapper extends PureComponent {
+    state = { shouldRender: false }
+
+    componentDidMount () {
+      raf(() => raf(() => this.setState({ shouldRender: true })))
     }
 
-    componentDidMount() {
-      raf(() => {
-        raf(() => this.setState({ shouldRender: true }))
-      })
-    }
-
-    render() {
+    render () {
       return this.state.shouldRender ? <WrappedComponent {...this.props} /> : null
     }
   }
+
+  return hoistNonReactStatic(DeferredRenderWrapper, WrappedComponent)
 }
